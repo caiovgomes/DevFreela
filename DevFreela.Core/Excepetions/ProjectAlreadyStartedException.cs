66ef9145f31b4151b9be5bdfd8563b2d452fa796
Caio Vitor <caiocogomes@hotmail.com>
2022-10-26 22:18:00 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.Core/Excepetions/ProjectAlreadyStartedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Excepetions
+{
+    public class ProjectAlreadyStartedException : Exception
+    {
+        public ProjectAlreadyStartedException() : base("Project is alredy in Started status")
+        {
+        }
+    }
+}
